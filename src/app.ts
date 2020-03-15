aba484f7e3f8b4f26e4161839f09dd9da6632fca
chacham <ccm0702@naver.com>
2020-03-15 23:23:37 +0900

    Add simple express app with ts

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import logger from 'morgan';
+import http from 'http';
+
+import indexRouter from './routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(indexRouter);
+
+const port = Number(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
