780bf8b9c12a8c846bf876171c9df15cddff7cc9
chacham <ccm0702@naver.com>
2020-04-01 22:14:51 +0900

    Use redis pub/sub for message

--- src/socket-handler/message-handler.ts
+++ src/socket-handler/message-handler.ts
@@ -1,3 +1,5 @@
+import redisClient from '../redis';
+
 interface Message {
   timestamp: number;
   message: string;
@@ -7,6 +9,11 @@ export default function (io: SocketIO.Server): void {
   io.on('connect', (socket: SocketIO.Socket) => {
     socket.on('message', (msg: Message) => {
       io.emit('message', msg);
+      redisClient.publish('message', JSON.stringify(msg));
+    });
+
+    redisClient.on('message', (channel, message) => {
+      io.emit('message', JSON.parse(message));
     });
   });
 }
